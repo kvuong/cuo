3c0133c0971d12cc31632969705b0986d3666aa3
Khoa Vuong <khoa.vuong@auspost.com.au>
2017-08-16 19:30:05 +1000

    Update wordpress db details.

--- web/wordpress/wp-config.php
+++ web/wordpress/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'cuo');
+define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'wordpress');
+define('DB_USER', 'dev');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'P@ssw0rd');
+define('DB_PASSWORD', 'dev');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
