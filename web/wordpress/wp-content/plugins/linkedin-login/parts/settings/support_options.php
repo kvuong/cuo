a18e6e16f2317ae25dc424cb205995554281683a
KV-MSI\Khoa Vuong <khoadvuong@gmail.com>
2017-08-22 00:29:47 +1000

    Install Linkedin login.

---
+++ web/wordpress/wp-content/plugins/linkedin-login/parts/settings/support_options.php
@@ -0,0 +1,15 @@
+<?php 
+/*
+ Title: Documentation
+ Setting: pkli_basic_options
+ Tab: Help & Support
+ Tab Order: 20
+ */
+
+?>
+
+<a href="http://thoughtengineer.com/wordpress-linkedin-login-plugin/">Plugin Documentation</a><br/><br/>
+
+<a href="http://thoughtengineer.com/wordpress-linkedin-login-plugin/">Request Support</a><br/><br/>
+
+<a href="http://thoughtengineer.com/contact/">Building A Startup Platform? Hire Us</a>
